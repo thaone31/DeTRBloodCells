263631246727cf29b53300fa101e1f7ccf427b4a
thaone31 <99576488+thaone31@users.noreply.github.com>
2024-02-07 17:11:01 +0700

    Update install.py

--- install.py
+++ install.py
@@ -1,3 +1,7 @@
-pip install -i https://test.pypi.org/simple/ supervision==0.3.0 -q transformers pytorch-lightning roboflow timm
+pip install -i https://test.pypi.org/simple/ supervision==0.3.0 
+pip install -q transformers   
+pip install -q pytorch-lightning
+pip install -q roboflow
+pip install -q timm
 
 nvidia-smi 
