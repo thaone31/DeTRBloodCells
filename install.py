a99e77f5bad5fc634be08a05d481b989b5d087ed
thaone31 <99576488+thaone31@users.noreply.github.com>
2024-02-07 17:15:07 +0700

    Update install.py

--- install.py
+++ install.py
@@ -1,5 +1,5 @@
 pip install -i https://test.pypi.org/simple/ supervision==0.3.0
-pip install -q transformers
-pip install -q pytorch-lightning
-pip install -q roboflow
-pip install -q timm
+# pip install -q transformers
+# pip install -q pytorch-lightning
+# pip install -q roboflow
+# pip install -q timm
