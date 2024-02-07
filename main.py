ff7a3c5da202978d1a86985d05b0c33c669424b2
thaone31 <99576488+thaone31@users.noreply.github.com>
2024-02-07 17:18:06 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ HOME = os.getcwd()
 print(HOME)
 
 import torch
-!nvcc --version
 TORCH_VERSION = ".".join(torch.__version__.split(".")[:2])
 CUDA_VERSION = torch.__version__.split("+")[-1]
 print("torch: ", TORCH_VERSION, "; cuda: ", CUDA_VERSION)
